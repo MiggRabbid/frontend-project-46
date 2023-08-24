4cd498109fbe403581090d3ee40e471f9282c5b5
MiggRabbid <mig26@inbox.ru>
2023-08-24 10:25:47 +0300

    fix gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -3,7 +3,8 @@ import { program } from 'commander';
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format')
-  .argument('<filepath1> filepath2>');
+  .argument('<filepath1> filepath2>')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format');
 
-program.parse(process.argv);
+program.parse();
