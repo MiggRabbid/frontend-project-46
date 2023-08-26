8c7161f85aaabfe7172dcfc8a7b6c9207be2c4f3
MiggRabbid <mig26@inbox.ru>
2023-08-26 09:18:19 +0300

    add Test Coverage

--- src/index.js
+++ src/index.js
@@ -4,33 +4,22 @@ import readFile from './utils.js';
 const genDiffTree = (file1, file2) => {
   let keysFromFiles = Object.keys(file1).concat(Object.keys(file2)).sort();
   keysFromFiles = keysFromFiles.filter((key, index) => keysFromFiles.indexOf(key) === index);
+  
   const deffTree = {};
 
   keysFromFiles.map((key) => {
     if (_.has(file1, key) && _.has(file2, key) && _.isEqual(file1[key], file2[key])) {
-      deffTree[key] = {
-        value: file1[key],
-        symbol: ' ',
-      };
+      deffTree[key] = { value: file1[key], symbol: ' ' };
     } else if (_.has(file1, key) && _.has(file2, key) && !_.isEqual(file1[key], file2[key])) {
-      deffTree[key] = {
-        value1: file1[key],
-        value2: file2[key],
-        symbol: '-+',
-      };
+      deffTree[key] = { value1: file1[key], value2: file2[key], symbol: '-+' };
     } else if (_.has(file1, key) && !_.has(file2, key)) {
-      deffTree[key] = {
-        value: file1[key],
-        symbol: '-',
-      };
+      deffTree[key] = { value: file1[key], symbol: '-' };
     } else if (!_.has(file1, key) && _.has(file2, key)) {
-      deffTree[key] = {
-        value: file2[key],
-        symbol: '+',
-      };
+      deffTree[key] = { value: file2[key], symbol: '+' };
     }
     return deffTree;
   });
+
   return deffTree;
 };
 
