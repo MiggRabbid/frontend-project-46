d1ce0ec303376d66213b4cd35029823057616494
MiggRabbid <mig26@inbox.ru>
2023-09-05 14:49:10 +0300

    trying to fix hexlet-check - 1

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .option('-f, --format <type>', 'output format')
   .action((filepath1, filepath2) => {
     const diff = genDiff(filepath1, filepath2, program.opts().format);
     console.log(diff);
